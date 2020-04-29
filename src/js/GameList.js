074f35c61b4e7c8bdd9dc3ed2985f74857964730
Yousri <laifayousri@gmail.com>
2020-04-29 13:55:51 +0200

    edit GameList.js

--- src/js/GameList.js
+++ src/js/GameList.js
@@ -1,5 +1,46 @@
 const GameList = (argument = "") => {
-    console.log("Game List", argument);
+    const preparePage = () => {
+        let cleanedArgument = argument.replace(/\s+/g, "-");
+        console.log(cleanedArgument)
+        let articles = "";
+
+        const fetchList = (url, argument) => {
+            let finalURL = url;
+            if (argument) {
+                finalURL = url + "?search=" + argument;
+            }
+
+            fetch(`${finalURL}`)
+                .then((response) => response.json())
+                .then((response) => {
+                    response.results.forEach((article) => {
+                        articles += `
+                    <div class="cardGame">
+                      <h1>${article.name}</h1>
+                      <h2>${article.released}</h2>
+                      <img src='${article.backround_image}' alt=''width="130" height="150" />
+                      <a href = "#gamedetail/${article.id}">${article.id}</a>
+                    </div>
+                  `;
+                    });
+                    document.querySelector(".page-list .articles").innerHTML = articles;
+                });
+        };
+
+        fetchList("https://api.rawg.io/api/games", cleanedArgument);
+    };
+
+    const render = () => {
+        gameContent.innerHTML = `
+        <section class="page-list">
+          <div class="articles">...loading</div>
+        </section>
+      `;
+
+        preparePage();
+    };
+
+    render();
 };
 
 export default GameList;
