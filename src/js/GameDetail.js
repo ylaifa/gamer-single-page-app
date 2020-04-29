d9b673fec2045c486ee9abc24dca324ecb86d0e0
Yousri <laifayousri@gmail.com>
2020-04-29 13:55:39 +0200

    edit GameDetail.js

--- src/js/GameDetail.js
+++ src/js/GameDetail.js
@@ -1,5 +1,46 @@
-const GameDetail = (argument = "") => {
-    console.log("Game Detail", argument);
+const GameDetail = (argument) => {
+    const preparePage = () => {
+        let cleanedArgument = argument.replace(/\s+/g, "-");
+        let articleContent = "";
+
+        const fetchGame = (url, argument) => {
+            let finalURL = url + argument;
+
+            fetch(`${finalURL}`)
+                .then((response) => response.json())
+                .then((response) => {
+                    let {
+                        name,
+                        released,
+                        description
+                    } = response;
+
+                    let articleDOM = document.querySelector(".page-detail .article");
+
+                    articleDOM.querySelector("h1.title").innerHTML = name;
+                    articleDOM.querySelector("p.release-date span").innerHTML = released;
+                    articleDOM.querySelector("p.description").innerHTML = description;
+                });
+        };
+
+        fetchGame("https://api.rawg.io/api/games/", cleanedArgument);
+    };
+
+    const render = () => {
+        gameContent.innerHTML = `
+        <section class="page-detail">
+          <div class="article">
+            <h1 class="title"></h1>
+            <p class="release-date">Release date : <span></span></p>
+            <p class="description"></p>
+          </div>
+        </section>
+      `;
+
+        preparePage();
+    };
+
+    render();
 };
 
 export default GameDetail;
