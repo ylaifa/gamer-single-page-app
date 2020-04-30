7fb7ff682e840a0d2aa941c6190c8ec21b696da2
Yousri <laifayousri@gmail.com>
2020-04-30 22:57:27 +0200

    edit Home.js

--- src/js/Home.js
+++ src/js/Home.js
@@ -1,7 +1,10 @@
 import moment from "moment";
 
-const Home = (argument = "") => {
+let pageSizeNumber = 9;
+
+const Home = (platform = "") => {
     document.getElementById("select-box").hidden = false;
+    console.log(platform)
     let time = moment().format().slice(0, 10);
     let future_time = moment().add(1, 'years').format().slice(0, 10);
 
@@ -10,13 +13,13 @@ const Home = (argument = "") => {
 
         const fetchList = (url) => {
             let finalURL = url;
-
-            console.log(finalURL);
+            if (platform != "") {
+                finalURL = url + "&platforms=" + platform;
+            }
 
             fetch(`${finalURL}`)
                 .then((response) => response.json())
                 .then((response) => {
-                    console.log(response);
                     return response;
                 })
                 .then((response) => {
@@ -49,10 +52,11 @@ const Home = (argument = "") => {
                 `;
                     });
                     document.querySelector(".page-list .articles").innerHTML = articles;
+
                 });
         };
 
-        fetchList(`https://api.rawg.io/api/games?dates=${time},${future_time}&ordering=-added&page_size=9`);
+        fetchList(`https://api.rawg.io/api/games?dates=${time},${future_time}&ordering=-added&page_size=${pageSizeNumber}`);
     };
 
     const render = () => {
@@ -68,79 +72,26 @@ const Home = (argument = "") => {
     render();
 };
 
-var x, i, j, selElmnt, a, b, c;
-/* Look for any elements with the class "custom-select": */
-x = document.getElementsByClassName("custom-select");
-for (i = 0; i < x.length; i++) {
-    selElmnt = x[i].getElementsByTagName("select")[0];
-    /* For each element, create a new DIV that will act as the selected item: */
-    a = document.createElement("DIV");
-    a.setAttribute("class", "select-selected");
-    a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-    x[i].appendChild(a);
-    /* For each element, create a new DIV that will contain the option list: */
-    b = document.createElement("DIV");
-    b.setAttribute("class", "select-items select-hide");
-    for (j = 1; j < selElmnt.length; j++) {
-        /* For each option in the original select element,
-        create a new DIV that will act as an option item: */
-        c = document.createElement("DIV");
-        c.innerHTML = selElmnt.options[j].innerHTML;
-        c.addEventListener("click", function (e) {
-            /* When an item is clicked, update the original select box,
-            and the selected item: */
-            var y, i, k, s, h;
-            s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-            h = this.parentNode.previousSibling;
-            for (i = 0; i < s.length; i++) {
-                if (s.options[i].innerHTML == this.innerHTML) {
-                    s.selectedIndex = i;
-                    h.innerHTML = this.innerHTML;
-                    y = this.parentNode.getElementsByClassName("same-as-selected");
-                    for (k = 0; k < y.length; k++) {
-                        y[k].removeAttribute("class");
-                    }
-                    this.setAttribute("class", "same-as-selected");
-                    break;
-                }
-            }
-            h.click();
-        });
-        b.appendChild(c);
-    }
-    x[i].appendChild(b);
-    a.addEventListener("click", function (e) {
-        /* When the select box is clicked, close any other select boxes,
-        and open/close the current select box: */
-        e.stopPropagation();
-        closeAllSelect(this);
-        this.nextSibling.classList.toggle("select-hide");
-        this.classList.toggle("select-arrow-active");
-    });
-}
+// Select Platform
+document.querySelector("select").addEventListener('change', (event) => {
+    let platform = document.querySelector("select").value;
+    Home(platform);
+});
 
-function closeAllSelect(elmnt) {
-    /* A function that will close all select boxes in the document,
-    except the current select box: */
-    var x, y, i, arrNo = [];
-    x = document.getElementsByClassName("select-items");
-    y = document.getElementsByClassName("select-selected");
-    for (i = 0; i < y.length; i++) {
-        if (elmnt == y[i]) {
-            arrNo.push(i)
-        } else {
-            y[i].classList.remove("select-arrow-active");
-        }
-    }
-    for (i = 0; i < x.length; i++) {
-        if (arrNo.indexOf(i)) {
-            x[i].classList.add("select-hide");
-        }
+// See More Page Size
+
+const pageSize = () => {
+    if (pageSizeNumber < 18) {
+        pageSizeNumber += 9;
+        Home();
+    } else {
+        pageSizeNumber += 9;
+        document.getElementById('see-more-button').hidden = true;
+        Home();
     }
 }
 
-/* If the user clicks anywhere outside the select box,
-then close all select boxes: */
-document.addEventListener("click", closeAllSelect);
+document.getElementById('see-more-button').addEventListener('click', pageSize)
+
 
 export default Home;
